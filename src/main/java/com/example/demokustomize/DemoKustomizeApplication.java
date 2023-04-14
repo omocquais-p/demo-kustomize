a6e0a8892957c0237d7cac2860e7772361d65502
Olivier Mocquais <omocquais@vmware.com>
2023-04-14 09:51:45 +0200

    demo kustomize

---
+++ src/main/java/com/example/demokustomize/DemoKustomizeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demokustomize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoKustomizeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoKustomizeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/demokustomize/DemoKustomizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demokustomize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoKustomizeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
